eef26284f5ead7130af13176c058e3bab1c3c946
Anisin Mikhail <anisin.miha@gmail.com>
2025-04-27 17:36:59 +0300

    Update send.js

--- api/send.js
+++ api/send.js
@@ -3,8 +3,8 @@ import fetch from "node-fetch";
 export default async function handler(req, res) {
   // Разрешаем CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   // Обрабатываем preflight-запрос
   if (req.method === "OPTIONS") {
